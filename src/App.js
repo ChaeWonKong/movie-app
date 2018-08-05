3cd2ef1fdeeab4083ab2a6c489cfa29060243f5f
공채원 <chaewonkong@gmail.com>
2018-08-04 20:19:17 -0700

    Add movie titles and images as props

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Movie from './Movie';
+
+const movieTitles = [
+  "Matrix",
+  "Dallas Buyer's Club",
+  "Star Wars",
+  "Gran Torino",
+]
+
+const movieImages = [
+  "https://images-na.ssl-images-amazon.com/images/I/51EG732BV3L._SY445_.jpg",
+  "https://m.media-amazon.com/images/M/MV5BMTYwMTA4MzgyNF5BMl5BanBnXkFtZTgwMjEyMjE0MDE@._V1_UX182_CR0,0,182,268_AL_.jpg",
+  "https://imgc.allpostersimages.com/img/posters/star-wars-return-of-the-jedi_u-L-EZYVJ0.jpg?src=gp&w=300&h=375",
+  "https://upload.wikimedia.org/wikipedia/en/thumb/c/c6/Gran_Torino_poster.jpg/220px-Gran_Torino_poster.jpg"
+]
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Movie title={movieTitles[0]} poster={movieImages[0]} />
+        <Movie title={movieTitles[1]} poster={movieImages[1]} />
+        <Movie title={movieTitles[2]} poster={movieImages[2]} />
+        <Movie title={movieTitles[3]} poster={movieImages[3]} />
       </div>
-    );
+    )
   }
 }
 
