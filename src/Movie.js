989b9193e9700ef5311108336567ffad3f55ecb4
공채원 <chaewonkong@gmail.com>
2018-08-16 16:53:22 +0900

    Change in css

--- src/Movie.js
+++ src/Movie.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Movie.css';
 import PropTypes from 'prop-types';
+import LineEllipsis from 'react-lines-ellipsis';
 
 // class Movie extends Component {
 
@@ -31,28 +32,57 @@ import PropTypes from 'prop-types';
 //     }
 // }
 
-function Movie({title, poster}) {
+function Movie({title, poster, genres, synopsis}) {
     return (
-        <div>
-            <MoviePoster poster={poster}/>
-            <h1>{title}</h1>
+        <div className="Movie">
+            <div className="Movie__column">
+                <MoviePoster poster={poster} alt={title} />
+            </div>
+            <div className="Movie__column">
+                <h1>{title}</h1>
+                <div className="Movie__genres">
+                    {genres.map((genre, index) => <MovieGenre genre={genre} key={index} />)}
+                </div>
+                <div className="Movie__synopsis">
+                <LineEllipsis
+                    text={synopsis} 
+                    maxLine='3'
+                    elipsis='...'
+                    trimRight
+                    basedOn='letters'
+                />
+                </div>
+            </div>
         </div>
     );
 }
 
-function MoviePoster({poster}) {
-    return(
-        <img src={poster} />
+function MovieGenre({genre}) {
+    return (
+        <span className="Movie__genre">{genre}</span>
     )
+}
+
+function MoviePoster({poster, alt}) {
+    return(
+        <img src={poster} alt={alt} title={alt} className="Movie__poster"/>
+    )
+}
+
+MovieGenre.propTypes = {
+    genre: PropTypes.string.isRequired
 }
 
 Movie.propTypes = {
     title: PropTypes.string.isRequired,
     poster: PropTypes.string.isRequired,
+    genres: PropTypes.array.isRequired,
+    synopsis: PropTypes.string.isRequired
 }
 
 MoviePoster.propTypes = {
     poster: PropTypes.string.isRequired,
+    alt: PropTypes.string.isRequired,
 }
 
 export default Movie;
