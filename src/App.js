1f7b9f248370abc7b1fff368be178776754eb063
공채원 <chaewonkong@gmail.com>
2018-08-16 17:36:42 +0900

    Deploy on github pages

--- src/App.js
+++ src/App.js
@@ -33,7 +33,12 @@ class App extends Component {
   _callApi = () => {
     return fetch("https://yts.am/api/v2/list_movies.json?sort_by=download_count")
     .then(res => res.json())
-    .then(json => json.data.movies)
+    // .then(json => json.data.movies)
+    .then(function(json) {
+      const result = json.data.movies;
+      console.log(result);
+      return result;
+    })
     .catch(err => console.log(err))
   }
 
